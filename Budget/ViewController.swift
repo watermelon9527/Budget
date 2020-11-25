62f80a061de85f90ad80bfd3954ee4d41d8e329c
nonochan <nono850226@gmail.com>
2020-11-25 15:55:21 +0800

    first commit

---
+++ Budget/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Budget
+//
+//  Created by nono chan  on 2020/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
