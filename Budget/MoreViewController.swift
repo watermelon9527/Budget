9a7d8498a0e57eb1a4461d115c8ec9b0f860132b
nonochan <nono850226@gmail.com>
2020-12-01 17:16:21 +0800

    build profile page

--- Budget/MoreViewController.swift
+++ Budget/MoreViewController.swift
@@ -2,7 +2,7 @@
 //  MoreViewController.swift
 //  Budget
 //
-//  Created by nono chan  on 2020/11/27.
+//  Created by nono chan  on 2020/12/1.
 //
 
 import UIKit
@@ -12,6 +12,18 @@ class MoreViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Do any additional setup after loading the view.
     }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
