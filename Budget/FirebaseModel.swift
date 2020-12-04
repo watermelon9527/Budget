6d2001fcc6ec0e1a037d9468ee6e18a1aadd0eb7
nonochan <nono850226@gmail.com>
2020-12-04 17:59:42 +0800

    firebase

---
+++ Budget/FirebaseModel.swift
@@ -0,0 +1,15 @@
+//
+//  FirebaseModel.swift
+//  Budget
+//
+//  Created by nono chan  on 2020/12/4.
+//
+
+import Foundation
+import FirebaseCore
+import FirebaseFirestore
+class Firestore {
+
+}
+
+
--- Budget/RecordViewController.swift
+++ Budget/RecordViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 class RecordViewController: UIViewController {
 
     @IBOutlet weak var amountTextField: UITextField!
+
+    @IBOutlet weak var foodButton: UIButton!
+    @IBOutlet weak var drinkButton: UIButton!
+    @IBOutlet weak var entertainButton: UIButton!
+    @IBOutlet weak var trafficButton: UIButton!
+    @IBOutlet weak var consumeButton: UIButton!
+    @IBOutlet weak var houseHoldButton: UIButton!
+    @IBOutlet weak var medicalButton: UIButton!
+    @IBOutlet weak var othersButton: UIButton!
+    @IBOutlet weak var incomeButton: UIButton!
+
+    @IBAction func confimButton(_ sender: Any) {
+        amountTextField.text = "0"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.isHidden = true
