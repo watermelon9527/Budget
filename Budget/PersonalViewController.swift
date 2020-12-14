38d830cedfb35951f316b3d50a7feddffbbbf7c0
nonochan <nono850226@gmail.com>
2020-12-14 23:06:14 +0800

    persomal page pickerview

--- Budget/PersonalViewController.swift
+++ Budget/PersonalViewController.swift
@@ -9,25 +9,46 @@ import UIKit
 
 class PersonalViewController: UIViewController {
 
-    @IBOutlet weak var personalTableView: UITableView!
+    @IBOutlet weak var timeTextField: UITextField!
+    @IBOutlet weak var budgetTextField: UITextField!
+    @IBOutlet weak var categoryTextField: UITextField!
+    let picker0 = UIPickerView()
+    let picker1 = UIPickerView()
+
+    let time = ["月", "週", "日"]
+    let category = ["食物", "飲品", "娛樂", "交通", "消費", "家用", "醫藥", "收入", "其他"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        personalTableView.dataSource = self
-        personalTableView.delegate = self
+        picker0.delegate = self
+        picker0.tag = 0
+
+        picker1.delegate = self
+        picker1.tag = 1
+        timeTextField.inputView = picker0
+        categoryTextField.inputView = picker1
     }
 }
-extension PersonalViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+extension PersonalViewController:UIPickerViewDelegate,UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = personalTableView.dequeueReusableCell(withIdentifier: "PersonalTableViewCell", for: indexPath) as! PersonalTableViewCell
-        return cell
-    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if picker0.tag == 1 {
+            return time.count
+        }
+        return category.count
 
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 375
     }
-
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if picker0.tag == 1 {
+        return time[row]
+        }
+        return category[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        timeTextField.text = time[row]
+        categoryTextField.text = category[row]
+    }
 }
